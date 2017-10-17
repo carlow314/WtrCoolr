eab74e70815d71bbce268c784514558bbf820f1d
Jeffrey Carlow <carlow314@Jeffreys-MacBook-Air.local>
2017-10-17 06:42:12 -0600

    NAV bar changes

--- src/router/approuter.js
+++ src/router/approuter.js
@@ -89,12 +89,18 @@ function PrivateRoute ({component: Component, authed, user, ...rest}) {
 
               <ul className="nav navbar-nav pull-right">
                   {this.state.authed
-                    ? <button
+                    ?
+                    <div> 
+                     <Link to="/dashboard" className="navbar-brand">Dashboard</Link>
+                     <Link to="/profile" className="navbar-brand">Profile</Link>  
+                    <button
                         style={{border: 'none', background: 'transparent'}}
                         onClick={() => {
                           logout()
                         }}
                         className="navbar-brand">Logout</button>
+                    </div>
+
                     :
                     <div className="navigationbuttons">
                       <span>
