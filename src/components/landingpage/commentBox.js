c72dabdba2142bca0729fb449a6465dc13287366
Jeffrey Carlow <carlow314@Jeffreys-MacBook-Air.local>
2017-10-21 07:37:06 -0600

    file clean up

--- src/components/landingpage/commentBox.js
+++ src/components/landingpage/commentBox.js
@@ -7,202 +7,232 @@ import style from '../style';
 import styles from './comment.css';
 
 class CommentBox extends Component {
- constructor(props) {
- super(props);
- this.state = { data: [] };
- this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
- this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
- this.handleCommentDelete = this.handleCommentDelete.bind(this);
- this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
- }
- loadCommentsFromServer() {
- axios.get(this.props.url)
- .then(res => {
- this.setState({ data: res.data });
- })
- }
- handleCommentSubmit(comment) {
- let comments = this.state.data;
- comment.id = Date.now();
- let newComments = comments.concat([comment]);
- this.setState({ data: newComments });
- axios.post(this.props.url, comment)
- .catch(err => {
- console.error(err);
- this.setState({ data: comments });
- });
- }
- handleCommentDelete(id) {
- axios.delete(`${this.props.url}/${id}`)
- .then(res => {
- console.log('Comment deleted');
- })
- .catch(err => {
- console.error(err);
- });
- }
- handleCommentUpdate(id, comment) {
- //sends the comment id and new text to our api
- axios.put(`${this.props.url}/${id}`, comment)
- .catch(err => {
- console.log(err);
- })
- }
- componentDidMount() {
- this.loadCommentsFromServer();
- setInterval(this.loadCommentsFromServer, 2000);
- }
- render() {
- return (
-   <div className="container commentformBox">
+	constructor(props) {
+		super(props);
+		this.state = { data: [] };
+		this.loadCommentsFromServer = this.loadCommentsFromServer.bind(this);
+		this.handleCommentSubmit = this.handleCommentSubmit.bind(this);
+		this.handleCommentDelete = this.handleCommentDelete.bind(this);
+		this.handleCommentUpdate = this.handleCommentUpdate.bind(this);
+	}
+	loadCommentsFromServer() {
+		axios.get(this.props.url).then(res => {
+			this.setState({ data: res.data });
+		});
+	}
+	handleCommentSubmit(comment) {
+		let comments = this.state.data;
+		comment.id = Date.now();
+		let newComments = comments.concat([comment]);
+		this.setState({ data: newComments });
+		axios.post(this.props.url, comment).catch(err => {
+			console.error(err);
+			this.setState({ data: comments });
+		});
+	}
+	handleCommentDelete(id) {
+		axios
+			.delete(`${this.props.url}/${id}`)
+			.then(res => {
+				console.log('Comment deleted');
+			})
+			.catch(err => {
+				console.error(err);
+			});
+	}
+	handleCommentUpdate(id, comment) {
+		//sends the comment id and new text to our api
+		axios.put(`${this.props.url}/${id}`, comment).catch(err => {
+			console.log(err);
+		});
+	}
+	componentDidMount() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, 2000);
+	}
+	render() {
+		return (
+			<div className="container commentformBox">
+				<div style={style.commentBox}>
+					<h2 style={style.title} />
+					<CommentForm onCommentSubmit={this.handleCommentSubmit} />
 
- <div style={ style.commentBox }>
- <h2 style={ style.title }></h2>
- <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
+					<div className="dropdown">
+						<button
+							className="btn btn-secondary dropdown-toggle dropdownBtn"
+							type="button"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="true"
+						>
+							SORT BY
+						</button>
+						<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a className="dropdown-item" href="#">
+								Most Popular
+							</a>
+							<a className="dropdown-item" href="#">
+								Hillarious
+							</a>
+							<a className="dropdown-item" href="#">
+								F**ed Up
+							</a>
+						</div>
+					</div>
 
- <div className="dropdown">
-   <button className="btn btn-secondary dropdown-toggle dropdownBtn" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-     SORT BY
-   </button>
-   <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-     <a className="dropdown-item" href="#">Most Popular</a>
-     <a className="dropdown-item" href="#">Hillarious</a>
-     <a className="dropdown-item" href="#">F**ed Up</a>
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<p>Me: “You’ll want to update your app to alleviate the issue.”</p>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <p>Me: “You’ll want to update your app to alleviate the issue.”</p>
+							<p>Customer: “How do I do that?”</p>
 
-      <p>Customer: “How do I do that?”</p>
+							<p>Me: “Just head to the app store and search for [App].”</p>
 
-      <p>Me: “Just head to the app store and search for [App].”</p>
+							<p>Customer: “I’m sorry; I simply don’t have time to drive to the app store today.”</p>
+							<LikeButton />
+						</div>
+					</div>
 
-      <p>Customer: “I’m sorry; I simply don’t have time to drive to the app store today.”</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<p>
+								Myself and two of my coworkers were walking out of the office to get lunch. S, has a
+								fairly large chest and M is half japanese and half irish. M, lacks any and all filters -{' '}
+							</p>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <p>Myself and two of my coworkers were walking out of the office to get lunch. S, has a fairly large chest and M is half japanese and half irish. M, lacks any and all filters - </p>
+							<p>
+								M: “So, S, have your boobs always been this big? (He asks this not in a sexual way, but
+								as though he was legitimately curious. Like the thought literally just crossed his mind
+								even though we have all been working together for awhile now).”
+							</p>
 
-<p>M: “So, S, have your boobs always been this big? (He asks this not in a sexual way, but as though he was legitimately curious. Like the thought literally just crossed his mind even though we have all been working together for awhile now).”</p>
+							<p>
+								S: “Uh, Yes, ALWAYS. Shoot, I’d be happy with a size B. I’ve had enough of this back
+								pain bs” (She nods her head, happy with the thought).
+							</p>
 
-<p>S: “Uh, Yes, ALWAYS. Shoot, I’d be happy with a size B. I’ve had enough of this back pain bs” (She nods her head, happy with the thought).</p>
+							<p>M: “Well, just so that you know - that’s still big in Asia land. </p>
 
-<p>M: “Well, just so that you know - that’s still big in Asia land. </p>
+							<p>
+								{' '}
+								Oh HR …. Not really, but this is a pretty indicative of conversations in the office….
+								Entertaining to overhear, but so politically incorrect.
+							</p>
+							<LikeButton />
+						</div>
+					</div>
 
-<p> Oh HR …. Not really, but this is a pretty indicative of conversations in the office…. Entertaining to overhear, but so politically incorrect.</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/image.png')} />
+							<p> Someone at work posted this little jem.</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/image.png')} />
-     <p> Someone at work posted this little jem.</p>
-     <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/checkflush.png')} />
+							<p>It really matters.</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/checkflush.png')} />
-     <p>It really matters.</p>
-     <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<p>M: Have you ever tried wearing a shock collar?</p>
+							<p>Me: Why no M, have you?</p>
+							<p>
+								M: Yes, if I had one I would bring it in so that you could try it. I think that everyone
+								should try it once (please don’t bring in a shock collar, M)
+							</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <p>M: Have you ever tried wearing a shock collar?</p>
-     <p>Me: Why no M, have you?</p>
-     <p>M: Yes, if I had one I would bring it in so that you could try it. I think that everyone should try it once
-       (please don’t bring in a shock collar, M)</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/jiminiesdead.png')} />
+							<p>Cricket Lives Matter</p>
+							<p>He's dead, Jim</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/jiminiesdead.png')} />
-     <p>Cricket Lives Matter</p>
-     <p>He's dead, Jim</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<p>
+								(I am a technician. I am fixing a blood pressure testing machine at the local
+								supermarket when a man comes up wanting to have his blood pressure checked.)
+							</p>
 
+							<p>Me: “Sorry, I’m not quite done here. I’ll have it up and running in a few minutes.” </p>
 
-<div className="allcomments">
-  <div className="commentImages">
-    <p>(I am a technician. I am fixing a blood pressure testing machine at the local supermarket when a man comes up wanting to have his blood pressure checked.)</p>
+							<p>
+								Customer: “You aren’t really fixing that; you’re just stealing the electricity to power
+								your laptop!”
+							</p>
 
-    <p>Me: “Sorry, I’m not quite done here. I’ll have it up and running in a few minutes.” </p>
+							<p>(My laptop is indeed sitting beside me, not even plugged into anything.)</p>
 
-    <p>Customer: “You aren’t really fixing that; you’re just stealing the electricity to power your laptop!”</p>
+							<p>Me: “No, I’m not.”</p>
 
-    <p>(My laptop is indeed sitting beside me, not even plugged into anything.)</p>
+							<p>Customer: “Would you like me to call a manager?”</p>
 
-    <p>Me: “No, I’m not.”</p>
+							<p>Me: “Please, do it!”</p>
 
-    <p>Customer: “Would you like me to call a manager?”</p>
+							<p>(He didn’t.)</p>
+						</div>
+					</div>
 
-    <p>Me: “Please, do it!”</p>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/jiminiesdead.png')} />
+							<p>Cricket Lives Matter</p>
+							<LikeButton />
+						</div>
+					</div>
 
-    <p>(He didn’t.)</p>
-  </div>
-</div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/flowers.png')} />
+							<p>Friends are sometimes the worst employees.</p>
+							<LikeButton />
+						</div>
+					</div>
 
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/nameonfood.png')} />
+							<p>What's mine is yours</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/jiminiesdead.png')} />
-     <p>Cricket Lives Matter</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/shypooper.png')} />
+							<p>They're watching, always watching.</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/flowers.png')} />
-     <p>Friends are sometimes the worst employees.</p>
-          <LikeButton />
-   </div>
- </div>
+					<div className="allcomments">
+						<div className="commentImages">
+							<img src={require('./images/whereareyou.png')} />
+							<p>Make sure you are where you say you are. We will find you.</p>
+							<LikeButton />
+						</div>
+					</div>
 
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/nameonfood.png')} />
-     <p>What's mine is yours</p>
-          <LikeButton />
-   </div>
- </div>
-
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/shypooper.png')} />
-     <p>They're watching, always watching.</p>
-          <LikeButton />
-   </div>
- </div>
-
- <div className="allcomments">
-   <div className="commentImages">
-     <img src={require('./images/whereareyou.png')} />
-     <p>Make sure you are where you say you are. We will find you.</p>
-          <LikeButton />
-   </div>
- </div>
-
- <CommentList
- onCommentDelete={ this.handleCommentDelete }
- onCommentUpdate={ this.handleCommentUpdate }
- data={ this.state.data }/>
- </div>
- </div>
- )
- }
+					<CommentList
+						onCommentDelete={this.handleCommentDelete}
+						onCommentUpdate={this.handleCommentUpdate}
+						data={this.state.data}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 export default CommentBox;
--- src/components/landingpage/commentform.js
+++ src/components/landingpage/commentform.js
@@ -3,36 +3,43 @@ import style from '../style';
 import styles from './comment.css';
 
 class CommentForm extends Component {
- constructor(props) {
- super(props);
- this.state = {text: '' };
- this.handleTextChange = this.handleTextChange.bind(this);
- this.handleSubmit = this.handleSubmit.bind(this);
- }
- handleTextChange(e) {
- this.setState({ text: e.target.value });
- }
- handleSubmit(e) {
- e.preventDefault();
- let text = this.state.text.trim();
- if (!text) {
- return;
- }
- this.props.onCommentSubmit({text: text });
- this.setState({text: '' });
- }
- selectEmoji(emoji) {
-    this.setState({emoji})
-  }
- render() {
- return (
-   <form onSubmit={ this.handleSubmit }>
-  <div className="form-group allPosts">
-    <input type="text" className="form-control" id="newComment" placeholder="Say something…" value={ this.state.text || this.state.emoji} onChange={ this.handleTextChange } />
-    <input type="submit"  id="commentformPost" value='POST'/>
-  </div>
-</form>
- )
- }
+	constructor(props) {
+		super(props);
+		this.state = { text: '' };
+		this.handleTextChange = this.handleTextChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleTextChange(e) {
+		this.setState({ text: e.target.value });
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		let text = this.state.text.trim();
+		if (!text) {
+			return;
+		}
+		this.props.onCommentSubmit({ text: text });
+		this.setState({ text: '' });
+	}
+	selectEmoji(emoji) {
+		this.setState({ emoji });
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<div className="form-group allPosts">
+					<input
+						type="text"
+						className="form-control"
+						id="newComment"
+						placeholder="Say something…"
+						value={this.state.text || this.state.emoji}
+						onChange={this.handleTextChange}
+					/>
+					<input type="submit" id="commentformPost" value="POST" />
+				</div>
+			</form>
+		);
+	}
 }
 export default CommentForm;
